95059806b5c1359cfd9d2716423b5a2ada576c3c
AlexKomanov <akomanov88@gmail.com>
2023-10-14 17:54:10 +0300

    Add components project

---
+++ 02-components-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+
+
+    return (
+        <>
+        <Header />
+        <Footer />
+        </>
+    )
+}
+
+export default App
