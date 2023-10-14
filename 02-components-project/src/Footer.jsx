b85fc867986a3cae2c14919c7e441edfbacc4234
AlexKomanov <akomanov88@gmail.com>
2023-10-14 17:54:42 +0300

    Adding footer and header components

---
+++ 02-components-project/src/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Alex Web Application</p>
+        </footer>
+    );
+
+}
+
+export default Footer
---
+++ 02-components-project/src/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+
+    return (
+        <header>
+            <h1>My Web Application</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
+
+
+export default Header
