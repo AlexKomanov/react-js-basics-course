3cfcfbf8268796e6cc0dc7b726d526b4aaa043e3
AlexKomanov <akomanov88@gmail.com>
2023-10-16 08:24:18 +0300

    Add props project

---
+++ 03-props-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Student from "./Student.jsx";
+
+
+function App() {
+
+
+  return (
+    <>
+        <h1>Props Lesson</h1>
+        <Student />
+        <Student />
+        <Student />
+    </>
+  )
+}
+
+export default App
---
+++ 03-props-project/src/Student.jsx
@@ -0,0 +1,13 @@
+
+const Student = () => {
+    return (
+        <div className="student">
+            <p>Name: Alex</p>
+            <p>Age: 35</p>
+            <p>Student: yes</p>
+
+        </div>
+    );
+}
+
+export default Student
